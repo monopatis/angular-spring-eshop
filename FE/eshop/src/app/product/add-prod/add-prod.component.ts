b1d10aef92a31400b6e230b6c1724ecff53191f4
Dimitris Monopatis <monopatis+git@gmail.com>
2019-10-20 00:22:41 +0300

    add product in FE

--- FE/eshop/src/app/product/add-prod/add-prod.component.ts
+++ FE/eshop/src/app/product/add-prod/add-prod.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { ProductService } from '../../services/product.service';
+import { NgForm } from '@angular/forms';
+
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-add-prod',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProdComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogbox: MatDialogRef<AddProdComponent>,
+    private service: ProductService,
+    private snackBar: MatSnackBar) { }
+
+  public listItems: Array<string> = [];
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+
+    this.service.formData = {
+      id: null,
+      name: '',
+      price: null,
+      description: '',
+      itemsLeft: null
+    }
+  }
+
+  onClose() {
+    this.dialogbox.close();
+    this.service.filter('Register click');
+  }
+
+  onSubmit(form: NgForm) {
+
+    this.service.addProduct(form.value).subscribe(res => {
+      this.onClose();
+      // this.resetForm(form);
+      // this.snackBar.open(res.firstName, '', {
+      //   duration: 5000,
+      //   verticalPosition: 'top'
+      // });
+    }
+    )
   }
 
 }
+
